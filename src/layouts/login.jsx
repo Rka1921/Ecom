940f40f4bd4afbb300ea799f24f006192cc29b78
Rka1921 <77318837+Rka1921@users.noreply.github.com>
2024-07-02 21:27:21 +0530

    small

--- src/layouts/login.jsx
+++ src/layouts/login.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import {Input} from "@nextui-org/react";
-import Lof from '../assets/bgLog.jpg'
+// import Lof from '../assets/bgLog.jpg'
 export const Login = () => {
 return(
     <>
     <div className="grid grid-cols-1 md:grid-cols-2 gap-44">
         <div className="w-80 h-20">
-        <img src={Lof} /></div>
+        {/* <img src={Lof} /></div> */}
       <div>
    <h1 className="mt-3">Login Page</h1>
     <div className="flex w-full flex-wrap gap-4 mt-4">
@@ -16,7 +16,9 @@ return(
     <Input type="password" label="Password" />
     </div>
   </div>
-  </div></>
-    
+  </div>
+  </div>
+  </>
+   
 )
 }
