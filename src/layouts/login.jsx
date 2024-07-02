1673e21e4f9c9e0fd26cfaae1dd17b88f1d5686b
Rka1921 <77318837+Rka1921@users.noreply.github.com>
2024-07-02 22:27:19 +0530

    Routing setup

--- src/layouts/login.jsx
+++ src/layouts/login.jsx
@@ -1,23 +1,27 @@
 import React from "react";
+import {Button} from "@nextui-org/react";
 import {Input} from "@nextui-org/react";
-// import Lof from '../assets/bgLog.jpg'
+import imga from '../assets/main.jpg'
 export const Login = () => {
 return(
     <>
-    <div className="grid grid-cols-1 md:grid-cols-2 gap-44">
+    <div className="grid grid-cols-1 gap-44 md:grid-cols-2">
         <div className="w-80 h-20">
-        {/* <img src={Lof} /></div> */}
-      <div>
-   <h1 className="mt-3">Login Page</h1>
-    <div className="flex w-full flex-wrap gap-4 mt-4">
+        <img src={imga} /></div>
+      <div className="flex flex-col gap-4">
+   <h1 className="mt-3">Login</h1>
+    <div>
     <Input type="email" label="Email" />
   </div>
-  <div className="flex w-full flex-wrap  gap-4 mt-4">
+  <div>
     <Input type="password" label="Password" />
     </div>
+    <Button className="bg-black col text-white">
+      Continue
+    </Button>
   </div>
   </div>
-  </div>
+ 
   </>
    
 )
