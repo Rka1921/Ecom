8bdc2a910734589c70f4dd391920bb303c1661d7
Rka1921 <77318837+Rka1921@users.noreply.github.com>
2024-07-03 21:44:32 +0530

    navigation added

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,5 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-
-import viteLogo from '/vite.svg'
 import './App.css'
-import { Login } from './layouts/login'
 import {NextUIProvider} from "@nextui-org/react";
 import { Routing } from './routes/Routes'
 
